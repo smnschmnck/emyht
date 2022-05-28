3bd821fb7ad3e5424562d8fa76e4ba018fa8c438
smnschmnck <simon.schmeinck@gmx.de>
2022-05-28 16:38:54 +0200

    added default fallback title for all pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        <title>emyht</title>
         <meta
           name='viewport'
           content='width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0'
