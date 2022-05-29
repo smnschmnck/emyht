b983f4073938817ae90ce0cd3bce394a5bc00760
smnschmnck <simon.schmeinck@gmx.de>
2022-05-29 15:07:55 +0200

    Handle errors on E-Mail verification

--- pages/verifyEmail/[id].tsx
+++ pages/verifyEmail/[id].tsx
@@ -3,34 +3,44 @@ import Head from 'next/head';
 import Link from 'next/link';
 import DefaultLayout from '../../components/DefaultLayout';
 import { BACKEND_HOST } from '../../helpers/globals';
+import styles from '../../styles/VerifiyEmailPage.module.css';
 
 interface EmailVerificationPageProps {
-  resText: string;
   verificationSuccess: boolean;
+  status: number;
 }
 
 export const getServerSideProps: GetServerSideProps<
   EmailVerificationPageProps
 > = async (context: GetServerSidePropsContext) => {
-  const { id } = context.query;
-  const verificationID = String(id);
-  const body = { emailToken: verificationID };
-  const response = await fetch(BACKEND_HOST + '/verifyEmail', {
-    method: 'post',
-    body: JSON.stringify(body),
-    headers: { 'Content-Type': 'application/json' },
-  });
-  return {
-    props: {
-      resText: await response.text(),
-      verificationSuccess: response.ok,
-    },
-  };
+  try {
+    const { id } = context.query;
+    const verificationID = String(id);
+    const body = { emailToken: verificationID };
+    const response = await fetch(BACKEND_HOST + '/verifyEmail', {
+      method: 'post',
+      body: JSON.stringify(body),
+      headers: { 'Content-Type': 'application/json' },
+    });
+    return {
+      props: {
+        verificationSuccess: response.ok,
+        status: response.status,
+      },
+    };
+  } catch {
+    return {
+      props: {
+        verificationSuccess: false,
+        status: 503,
+      },
+    };
+  }
 };
 
 const EmailVerificationPage: NextPage<EmailVerificationPageProps> = ({
   verificationSuccess,
-  resText,
+  status,
 }) => {
   return (
     <>
@@ -38,10 +48,41 @@ const EmailVerificationPage: NextPage<EmailVerificationPageProps> = ({
         <title>E-Mail Verification</title>
       </Head>
       <DefaultLayout>
-        <div>
-          {verificationSuccess && <h1>E-Mail succesfully verified</h1>}
-          {!verificationSuccess && <h1>{resText}</h1>}
-          <Link href={'/'}>Home</Link>
+        <div className={styles.content}>
+          <div className={styles.main}>
+            {verificationSuccess && (
+              <>
+                <h1>E-Mail verified successfully 🥳</h1>
+                <Link href={'/'}>
+                  <a className={styles.startLink}>Start using emyht</a>
+                </Link>
+              </>
+            )}
+            {!verificationSuccess && (
+              <>
+                {status === 404 && (
+                  <>
+                    <h1>Could not Verify E-Mail 😟</h1>
+                    <h2>Seems like the supplied link does not work</h2>
+                  </>
+                )}
+                {status === 409 && (
+                  <>
+                    <h1>E-Mail was already verified ✅</h1>
+                    <Link href={'/'}>
+                      <a className={styles.startLink}>Start using emyht</a>
+                    </Link>
+                  </>
+                )}
+                {status === 503 && (
+                  <>
+                    <h1>Error ⚠️</h1>
+                    <h2>Please check back later</h2>
+                  </>
+                )}
+              </>
+            )}
+          </div>
         </div>
       </DefaultLayout>
     </>
