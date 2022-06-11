6a25779d98d4dfc88dd778fd949e6f20e781e5f1
smnschmnck <simon.schmeinck@gmx.de>
2022-06-11 15:08:13 +0200

    check if 101 chat between to uuids exist via sql

--- chatService/chatService.go
+++ chatService/chatService.go
@@ -49,7 +49,35 @@ func StartOneOnOneChat(c *fiber.Ctx) error {
 
 	chatID := uuid.New().String()
 
-	//TODO: dont allow creation if chat exists
+	//Complex SQL Query ahead
+	/*
+		SELECT ALL one_on_one chats which belong to one of two UUIDs
+		If the same chat_id exists for both UUIDs (twice) it must mean
+		that a one_on_one chat must already exist between both uuids
+	*/
+	checkChatExistsQuery := "SELECT count(user_chat.chat_id) >= 2 AS chatcount " +
+		"FROM user_chat " +
+		"JOIN chats c on user_chat.chat_id = c.chat_id " +
+		"WHERE chat_type='one_on_one' " +
+		"AND (uuid=$1 OR uuid=(SELECT uuid FROM users WHERE email=$2)) " +
+		"GROUP BY c.chat_id " +
+		"ORDER BY chatcount DESC " +
+		"LIMIT 1"
+	var chatExists bool
+	err = conn.QueryRow(ctx, checkChatExistsQuery, reqUUID, req.ParticipantEmail).Scan(&chatExists)
+	if err != nil {
+		if err.Error() == "no rows in result set" {
+			chatExists = false
+		} else {
+			fmt.Print(err)
+			return c.Status(500).SendString("INTERNAL ERROR")
+		}
+	}
+
+	if chatExists {
+		return c.Status(409).SendString("CHAT EXISTS ALREADY")
+	}
+
 	var dbChatID string
 	createChatQuery := "INSERT INTO chats(chat_id, name, picture_url, chat_type) " +
 		"VALUES ($1, '', '','one_on_one') " +
