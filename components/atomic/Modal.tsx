5e835e590e33ab5eb9a3dac4a5e0e9ad9651db14
smnschmnck <simon.schmeinck@gmx.de>
2022-06-14 18:19:37 +0200

    added props to make modal fullscreen on mobile

--- components/atomic/Modal.tsx
+++ components/atomic/Modal.tsx
@@ -3,16 +3,27 @@ import styles from '../../styles/AtomicModal.module.css';
 interface ModalProps {
   children?: React.ReactNode;
   backgroundClickHandler?: () => void;
+  mobileFullscreen?: boolean;
 }
 
 export const Modal: React.FC<ModalProps> = ({
   children,
-  backgroundClickHandler: closeHandler,
+  backgroundClickHandler,
+  mobileFullscreen,
 }) => {
+  const isMobileFullscreen = mobileFullscreen ?? false;
   return (
     <span className={styles.modal}>
-      <span className={styles.foreground}>{children}</span>
-      <span className={styles.background} onClick={closeHandler}></span>
+      <span
+        className={styles.foreground}
+        id={isMobileFullscreen ? styles.mobileFullscreen : ''}
+      >
+        {children}
+      </span>
+      <span
+        className={styles.background}
+        onClick={backgroundClickHandler}
+      ></span>
     </span>
   );
 };
