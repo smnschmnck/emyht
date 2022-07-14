16fc0ad05dceae2d3e49d4cde0c6cef07851be88
smnschmnck <simon.schmeinck@gmx.de>
2022-07-15 01:48:43 +0200

    sending contact request to backend

--- components/ContactRequestModal.tsx
+++ components/ContactRequestModal.tsx
@@ -2,6 +2,7 @@ import { Modal } from './atomic/Modal';
 import styles from '../styles/ContactRequestModal.module.css';
 import { Input } from './atomic/Input';
 import { BigButton, SmallButton } from './atomic/Button';
+import { FormEvent, useState } from 'react';
 
 interface ContactRequestModalProps {
   closeHandler: () => void;
@@ -10,21 +11,63 @@ interface ContactRequestModalProps {
 export const ContactRequestModal: React.FC<ContactRequestModalProps> = ({
   closeHandler,
 }) => {
+  const [email, setEmail] = useState('');
+  const [requestSuccess, setrequestSuccess] = useState(false);
+  const [requestError, setRequestError] = useState('');
+
+  const submitHandler = async (e: FormEvent) => {
+    e.preventDefault();
+    const body = {
+      contactEmail: email,
+    };
+    const res = await fetch('/api/contactRequest', {
+      method: 'post',
+      body: JSON.stringify(body),
+    });
+    if (!res.ok) {
+      setRequestError(await res.text());
+      return;
+    }
+    setrequestSuccess(true);
+  };
+
   return (
     <Modal backgroundClickHandler={closeHandler} mobileFullscreen={true}>
       <div className={styles.container}>
-        <div className={styles.content}>
-          <h2 className={styles.heading}>Add contact 👋</h2>
-          <div className={styles.interactiveContent}>
-            <form className={styles.form} onSubmit={(e) => e.preventDefault()}>
-              <Input autoFocus placeholder="E-Mail address" />
-              <BigButton type="submit">Send</BigButton>
-            </form>
-            <SmallButton onClick={() => closeHandler()} type="button">
-              Cancel
-            </SmallButton>
+        {!requestSuccess && (
+          <div className={styles.content}>
+            <h2 className={styles.heading}>Add contact 👋</h2>
+            <div className={styles.interactiveContent}>
+              <form
+                className={styles.form}
+                onSubmit={submitHandler}
+                onChange={() => setRequestError('')}
+              >
+                <Input
+                  autoFocus
+                  placeholder="E-Mail address"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <BigButton type="submit">Send</BigButton>
+              </form>
+              {requestError && (
+                <p className={styles.requestError}>{requestError}</p>
+              )}
+              <SmallButton onClick={() => closeHandler()} type="button">
+                Cancel
+              </SmallButton>
+            </div>
           </div>
-        </div>
+        )}
+        {requestSuccess && (
+          <div className={styles.success}>
+            <h2 className={styles.successMessage}>
+              Request sent successfully 🥳
+            </h2>
+            <BigButton onClick={() => closeHandler()}>Close</BigButton>
+          </div>
+        )}
       </div>
     </Modal>
   );
---
+++ pages/api/contactRequest.ts
@@ -0,0 +1,27 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { BACKEND_HOST } from '../../helpers/globals';
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const sessionID = req.cookies.SESSIONID;
+  if (!sessionID) {
+    return res.status(401).send('NOT AUTHORIZED');
+  }
+
+  try {
+    const response = await fetch(`${BACKEND_HOST}/contactRequest`, {
+      headers: {
+        authorization: `Bearer ${sessionID}`,
+        'Content-Type': 'application/json',
+      },
+      method: 'post',
+      body: req.body,
+    });
+    const status = response.status;
+    const msg = await response.text();
+    return res.status(status).send(msg);
+  } catch (err) {
+    return res.status(500).send('COULD NOT SEND CONTACT REQUEST');
+  }
+};
+
+export default handler;
