d5ebedacf0bcaed633ab5ef6fb3e21bd214077cc
smnschmnck <simon.schmeinck@gmx.de>
2022-09-09 00:49:07 +0200

    Added styling to error

--- components/atomic/Error.tsx
+++ components/atomic/Error.tsx
@@ -1,9 +1,18 @@
 import styles from '../../styles/AtomicError.module.css';
+import Image from 'next/image';
+import warning from '../../assets/images/warning.svg';
 
 interface ErrorProps {
   errorMessage: string;
 }
 
 export const Error: React.FC<ErrorProps> = ({ errorMessage }) => {
-  return <p className={styles.errorP}>{errorMessage}</p>;
+  return (
+    <div className={styles.errorContainer}>
+      <div className={styles.errorContent}>
+        <Image src={warning} alt="Warning" />
+        <p className={styles.errorP}>{errorMessage}</p>
+      </div>
+    </div>
+  );
 };
