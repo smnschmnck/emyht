4b48fd22906ecde2d53e8aff02e9f6aab694e01c
smnschmnck <simon.schmeinck@gmx.de>
2022-09-29 01:19:28 +0200

    Show data from backend as chat info

--- components/ChatView.tsx
+++ components/ChatView.tsx
@@ -3,6 +3,7 @@ import { ChatMessageContainer } from './ChatMessageContainer';
 import { SendMessageForm } from './SendMessageForm';
 import styles from '../styles/ChatViewComponent.module.css';
 import ISingleChat from '../interfaces/ISingleChat';
+import { useQuery } from '@tanstack/react-query';
 
 interface ChatViewProps {
   chatID: string;
@@ -11,20 +12,33 @@ interface ChatViewProps {
   chats: ISingleChat[];
 }
 
+interface ChatInfo {
+  info: string;
+}
+
 export const ChatView: React.FC<ChatViewProps> = ({
   chatID,
   closeChat,
   chatOpened,
   chats,
 }) => {
+  const chatInfoQuery = useQuery(['chatInfo', chatID], async () => {
+    const res = await fetch(`/api/getChatInfo/${chatID}`);
+    if (!res.ok) {
+      throw new Error(await res.text());
+    }
+    return (await res.json()) as ChatInfo;
+  });
+  const chatInfoString = chatInfoQuery.data?.info ?? '';
   const curChat = chats.find((c) => c.chatID === chatID);
+
   return (
     <div className={styles.mainChat}>
       <ChatInfoHeader
         name={curChat?.chatName ?? ''}
         picUrl={curChat?.pictureUrl}
         close={closeChat}
-        info={'12:32'}
+        info={chatInfoString}
       />
       <div className={styles.chatContainer}>
         <div className={styles.messageContainer}>
