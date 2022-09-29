4ba990fc5e500c46ee3e2c470fa227d27aa19f11
smnschmnck <simon.schmeinck@gmx.de>
2022-09-29 15:56:53 +0200

    Basic styling of 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,6 +1,8 @@
 import { NextPage } from 'next';
 import Head from 'next/head';
+import { BigLink } from '../components/atomic/Link';
 import DefaultLayout from '../components/DefaultLayout';
+import styles from '../styles/NotFoundPage.module.css';
 
 const FourOhFour: NextPage = ({}) => {
   return (
@@ -9,8 +11,14 @@ const FourOhFour: NextPage = ({}) => {
         <title>404 - Page not found</title>
       </Head>
       <DefaultLayout>
-        <div>
-          <h1>404 - Page not found</h1>
+        <div className={styles.main}>
+          <div className={styles.container}>
+            <div className={styles.textContainer}>
+              <h1 className={styles.statusCode}>404</h1>
+              <h2 className={styles.description}>Page not found</h2>
+            </div>
+            <BigLink href={'/'}>Back to start</BigLink>
+          </div>
         </div>
       </DefaultLayout>
     </>
