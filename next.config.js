31eb88e137bb5a0e8f4e1c8703d11549a91e9b47
smnschmnck <simon.schmeinck@gmx.de>
2022-09-29 21:36:10 +0200

    added r2 bucket as image source

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['loremflickr.com'],
+    domains: ['emyht.3cf98ef84ef45348c295e56c0070c120.r2.cloudflarestorage.com'],
   },
 }
 
