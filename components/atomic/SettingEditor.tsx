be26e0e938f2b825d7272765c019ed4d6b63561f
smnschmnck <simon.schmeinck@gmx.de>
2022-09-30 23:20:46 +0200

    Added edit mode

--- components/atomic/SettingEditor.tsx
+++ components/atomic/SettingEditor.tsx
@@ -1,5 +1,7 @@
+import { useState } from 'react';
 import styles from '../../styles/AtomicSettingEditor.module.css';
 import { SmallButton } from './Button';
+import { Input } from './Input';
 
 interface SettingEditorProps {
   settingName: string;
@@ -10,14 +12,33 @@ export const SettingEditor: React.FC<SettingEditorProps> = ({
   settingName,
   settingValue,
 }) => {
+  const [editMode, setEditMode] = useState(false);
+  const [editVal, setEditVal] = useState(settingValue);
   return (
     <div className={styles.settingContainer}>
       <div className={styles.settingInfo}>
         <h4 className={styles.settingName}>{settingName}</h4>
-        <p className={styles.settingValue}>{settingValue}</p>
+        {!editMode && <p className={styles.settingValue}>{settingValue}</p>}
+        {editMode && (
+          <div className={styles.editor}>
+            <Input
+              value={editVal}
+              onChange={(e) => setEditVal(e.target.value)}
+              autoFocus
+            />
+            <div className={styles.editModeControls}>
+              <SmallButton>Save</SmallButton>
+              <SmallButton onClick={() => setEditMode(false)}>
+                Cancel
+              </SmallButton>
+            </div>
+          </div>
+        )}
       </div>
       <div className={styles.changeButton}>
-        <SmallButton>Change</SmallButton>
+        {!editMode && (
+          <SmallButton onClick={() => setEditMode(true)}>Change</SmallButton>
+        )}
       </div>
     </div>
   );
