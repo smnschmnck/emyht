e45842a6c88b1366786fc32a7726ea271fab96b1
smnschmnck <simon.schmeinck@gmx.de>
2023-11-07 20:06:37 +0100

    Run preview on port 3000

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 //@ts-ignore
-import "./src/env";
+import './src/env';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,10 +11,13 @@ export default defineConfig({
   server: {
     port: 3000,
   },
+  preview: {
+    port: 3000,
+  },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "@assets": path.resolve(__dirname, "./src/assets"),
+      '@': path.resolve(__dirname, './src'),
+      '@assets': path.resolve(__dirname, './src/assets'),
     },
   },
 });
