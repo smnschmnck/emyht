345baabc21c750110ad65d345eeacae4674acde3
smnschmnck <simon.schmeinck@gmx.de>
2023-11-10 19:44:36 +0100

    Use more fun shape for checkmark icon

--- frontend/src/assets/icons/CheckmarkIcon.tsx
+++ frontend/src/assets/icons/CheckmarkIcon.tsx
@@ -2,17 +2,16 @@ import { FC } from 'react';
 
 export const CheckMarkIcon: FC = () => (
   <svg
-    xmlns="http://www.w3.org/2000/svg"
+    className="h-2 w-2"
+    viewBox="0 0 8 7"
     fill="none"
-    viewBox="0 0 24 24"
-    strokeWidth={1.5}
-    stroke="currentColor"
-    className="h-6 w-6"
+    xmlns="http://www.w3.org/2000/svg"
   >
     <path
+      d="M0.875 3.8125L3.375 6.3125C3.375 6.3125 3.98806 4.86401 4.5 4C5.34137 2.58 7.125 0.6875 7.125 0.6875"
+      stroke="white"
       strokeLinecap="round"
       strokeLinejoin="round"
-      d="M4.5 12.75l6 6 9-13.5"
-    />
+    ></path>
   </svg>
 );
