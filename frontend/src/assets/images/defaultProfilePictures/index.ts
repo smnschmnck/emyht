3acf662b6a5cad763c801c80594150a078305af0
smnschmnck <simon.schmeinck@gmx.de>
2023-11-10 16:14:59 +0100

    Import default group and profile pictures

---
+++ frontend/src/assets/images/defaultProfilePictures/index.ts
@@ -0,0 +1,23 @@
+import pic0 from './0.svg';
+import pic1 from './1.svg';
+import pic2 from './2.svg';
+import pic3 from './3.svg';
+import pic4 from './4.svg';
+import pic5 from './5.svg';
+import pic6 from './6.svg';
+import pic7 from './7.svg';
+import pic8 from './8.svg';
+import pic9 from './9.svg';
+
+export const defaultProfilePictures = [
+  pic0,
+  pic1,
+  pic2,
+  pic3,
+  pic4,
+  pic5,
+  pic6,
+  pic7,
+  pic8,
+  pic9,
+];
