40db9cd27d0e1203634bb4fed65a704b8a78e8f7
smnschmnck <simon.schmeinck@gmx.de>
2023-11-10 19:49:43 +0100

    Add fallback image

--- frontend/src/components/ui/Avatar.tsx
+++ frontend/src/components/ui/Avatar.tsx
@@ -3,6 +3,21 @@ import * as AvatarPrimitive from '@radix-ui/react-avatar';
 import { defaultProfilePictures } from '@/assets/images/defaultProfilePictures';
 import { defaultGroupPictures } from '@/assets/images/defaultGroupPictures';
 
+const Fallback: FC = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="currentColor"
+    className="h-6 w-6 text-zinc-500"
+  >
+    <path
+      fillRule="evenodd"
+      d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z"
+      clipRule="evenodd"
+    />
+  </svg>
+);
+
 const formatPicURL = (profilePictureUrl?: string) => {
   if (!profilePictureUrl) {
     return;
@@ -38,7 +53,9 @@ export const Avatar: FC<AvatarProps> = ({ imgUrl, alt }) => {
         alt={alt}
         className="h-full w-full rounded-full object-cover"
       />
-      <AvatarPrimitive.Fallback delayMs={600}>XX</AvatarPrimitive.Fallback>
+      <AvatarPrimitive.Fallback delayMs={600}>
+        <Fallback />
+      </AvatarPrimitive.Fallback>
     </AvatarPrimitive.Root>
   );
 };
