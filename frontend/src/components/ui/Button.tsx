d0fb23c42dfb38eed870d6bd7cdc07c10529a96f
smnschmnck <simon.schmeinck@gmx.de>
2023-11-10 20:12:43 +0100

    Add mor button states

--- frontend/src/components/ui/Button.tsx
+++ frontend/src/components/ui/Button.tsx
@@ -1,19 +1,35 @@
+import { cva } from 'class-variance-authority';
 import { ButtonHTMLAttributes, FC } from 'react';
 import { twMerge } from 'tailwind-merge';
 
-type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+const buttonVariants = cva(
+  'h-10 rounded-md px-4 text-sm font-medium transition disabled:bg-zinc-100 disabled:text-zinc-200',
+  {
+    variants: {
+      variant: {
+        primary: 'bg-blue-500 text-white hover:bg-blue-400',
+        secondary: 'text-blue-500 bg-zinc-100 hover:bg-zinc-200',
+      },
+    },
+    defaultVariants: {
+      variant: 'primary',
+    },
+  }
+);
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  variant?: 'primary' | 'secondary';
+};
 
 export const Button: FC<ButtonProps> = ({
   children,
   type = 'button',
   className,
+  variant,
   ...props
 }) => (
   <button
-    className={twMerge(
-      'h-10 rounded-md bg-blue-600 px-4 text-sm font-medium text-white transition hover:bg-blue-500',
-      className
-    )}
+    className={twMerge(buttonVariants({ variant, className }))}
     type={type}
     {...props}
   >
--- frontend/src/pages/index/initiate/ChatCreator.tsx
+++ frontend/src/pages/index/initiate/ChatCreator.tsx
@@ -130,9 +130,14 @@ export const ChatCreator: FC = () => {
           </div>
           {chatMode === 'group' && (
             <div className="flex h-full w-full flex-col justify-between">
-              <div className="flex w-full items-center justify-center gap-2">
-                <Avatar className="h-14 min-h-[3.5rem] w-14 min-w-[3.5rem]" />
-                <Button className="w-full">Change group picture</Button>
+              <div className="flex w-full items-center justify-center gap-4">
+                <Avatar
+                  fallbackDelay={0}
+                  className="h-14 min-h-[3.5rem] w-14 min-w-[3.5rem]"
+                />
+                <Button className="w-full" variant="secondary">
+                  Change group picture
+                </Button>
               </div>
               <Input placeholder="Group name" />
               <Button disabled={!hasSelectedUsers}>Create group</Button>
