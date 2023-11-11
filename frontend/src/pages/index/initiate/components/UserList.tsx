e5d3889d43a16f6de1a50f212032cf7a8c5d2c97
smnschmnck <simon.schmeinck@gmx.de>
2023-11-11 18:41:22 +0100

    Load actual contacts

--- frontend/src/pages/index/initiate/components/UserList.tsx
+++ frontend/src/pages/index/initiate/components/UserList.tsx
@@ -1,42 +1,11 @@
-import { Contact } from '@/api/contacts';
 import { CheckMarkIcon } from '@/assets/icons/CheckmarkIcon';
 import { Avatar } from '@/components/ui/Avatar';
 import { Input } from '@/components/ui/Input';
+import { Spinner } from '@/components/ui/Spinner';
+import { queryKeys } from '@/configs/queryKeys';
+import { useQuery } from '@tanstack/react-query';
 import { FC, useState } from 'react';
 import { twMerge } from 'tailwind-merge';
-
-const fakeUsers: Contact[] = [
-  {
-    id: 'jsnkjdnf',
-    profilePictureUrl: 'default_1',
-    name: 'John Doe',
-  },
-  {
-    id: 'ubwetiwr',
-    profilePictureUrl: 'default_4',
-    name: 'John Doe',
-  },
-  {
-    id: 'jdskfbskjd',
-    profilePictureUrl: 'default_9',
-    name: 'John Doe',
-  },
-  {
-    id: 'o9b93r8b',
-    profilePictureUrl: 'default_1',
-    name: 'John Doe',
-  },
-  {
-    id: 'udbsifbd9',
-    profilePictureUrl: 'default_4',
-    name: 'Jane Doe',
-  },
-  {
-    id: 'ofsdnoin',
-    profilePictureUrl: 'default_9',
-    name: 'John Doe',
-  },
-];
 
 const SelectedIndicator: FC<{ selected: boolean }> = ({ selected }) => (
   <div
@@ -58,6 +27,7 @@ export const UserList: FC<UserListProps> = ({
   selectedUsers,
   setSelectedUsers,
 }) => {
+  const { data: contacts, isLoading } = useQuery(queryKeys.contacts.all);
   const [searchQuery, setSearchQuery] = useState('');
   const changeUser = (id: string) => {
     if (selectedUsers.includes(id)) {
@@ -73,7 +43,7 @@ export const UserList: FC<UserListProps> = ({
     setSearchQuery(query);
   };
 
-  const filteredUsers = fakeUsers.filter((user) => {
+  const filteredUsers = contacts?.filter((user) => {
     const curUsernameLowerCase = user.name.toLowerCase();
     const queryLowerCase = searchQuery.toLowerCase();
 
@@ -91,22 +61,30 @@ export const UserList: FC<UserListProps> = ({
       </div>
       <div className="h-full overflow-scroll">
         <ul className="pr-4">
-          {filteredUsers.map((user) => (
-            <li key={user.id}>
-              <button
-                onClick={() => changeUser(user.id)}
-                aria-label={`Add ${user.name} to chat`}
-                className="flex w-full items-center justify-between border-b border-b-zinc-100 p-2 transition hover:bg-zinc-100"
-              >
-                <div className="flex items-center gap-4">
-                  <Avatar imgUrl={user.profilePictureUrl} />
-                  <p className="text-sm font-semibold">{user.name}</p>
-                </div>
-                <SelectedIndicator selected={selectedUsers.includes(user.id)} />
-              </button>
-            </li>
-          ))}
+          {filteredUsers &&
+            filteredUsers.map((user) => (
+              <li key={user.id}>
+                <button
+                  onClick={() => changeUser(user.id)}
+                  aria-label={`Add ${user.name} to chat`}
+                  className="flex w-full items-center justify-between border-b border-b-zinc-100 p-2 transition hover:bg-zinc-100"
+                >
+                  <div className="flex items-center gap-4">
+                    <Avatar imgUrl={user.profilePictureUrl} />
+                    <p className="text-sm font-semibold">{user.name}</p>
+                  </div>
+                  <SelectedIndicator
+                    selected={selectedUsers.includes(user.id)}
+                  />
+                </button>
+              </li>
+            ))}
         </ul>
+        {isLoading && (
+          <div className="flex h-full w-full items-center justify-center py-8">
+            <Spinner />
+          </div>
+        )}
       </div>
     </div>
   );
