63169a74502c797c1a11de88a969973ced64f2fe
smnschmnck <simon.schmeinck@gmx.de>
2023-11-13 18:43:11 +0100

    Add function to accept or decline contact requests

--- frontend/src/assets/icons/NoEntryIcon.tsx
+++ frontend/src/assets/icons/NoEntryIcon.tsx
@@ -1,19 +1,19 @@
 import { FC } from 'react';
 import { twMerge } from 'tailwind-merge';
 
-export const CloseIcon: FC<{ className?: string }> = ({ className }) => (
+export const NoEntryIcon: FC<{ className?: string }> = ({ className }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
     viewBox="0 0 24 24"
     strokeWidth={1.5}
     stroke="currentColor"
-    className={twMerge('h-5 w-5', className)}
+    className={twMerge('h-6 w-6', className)}
   >
     <path
       strokeLinecap="round"
       strokeLinejoin="round"
-      d="M6 18L18 6M6 6l12 12"
+      d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636"
     />
   </svg>
 );
