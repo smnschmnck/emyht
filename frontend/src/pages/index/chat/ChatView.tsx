a6e6dcbe7a3aa5cc70c4c69331a877c472cc3475
smnschmnck <simon.schmeinck@gmx.de>
2023-12-03 18:41:27 +0100

    Remove unused code

--- frontend/src/pages/index/chat/ChatView.tsx
+++ frontend/src/pages/index/chat/ChatView.tsx
@@ -1,52 +1,11 @@
-import { ChatMessage } from '@/api/messages';
 import { queryKeys } from '@/configs/queryKeys';
-import { formatTimestamp } from '@/utils/dateUtils';
 import { useQuery } from '@tanstack/react-query';
 import { useLoader } from '@tanstack/react-router';
 import { FC, useEffect, useRef } from 'react';
+import { ChatHeader } from './components/ChatHeader';
 import { MessageInput } from './components/MessageInput';
 import { chatRoute } from './route';
-import { ChatHeader } from './components/ChatHeader';
-import { Spinner } from '@/components/ui/Spinner';
-
-const ChatMessage: FC<{ message: ChatMessage }> = ({ message }) => {
-  const { data } = useQuery(queryKeys.users.details);
-
-  if (!data) {
-    return (
-      <div className="flex h-full w-full items-center justify-center">
-        <Spinner />
-      </div>
-    );
-  }
-
-  if (data.uuid === message.senderID) {
-    return (
-      <li className="flex w-full flex-col items-end gap-1">
-        <span className="w-fit rounded-2xl bg-blue-600 px-2 py-1 text-sm text-white">
-          {message.textContent}
-        </span>
-        <span className="text-xs text-zinc-400">
-          {formatTimestamp(message.timestamp)}
-        </span>
-      </li>
-    );
-  }
-
-  return (
-    <li className="flex w-full flex-col items-start gap-1">
-      <div className="flex gap-2">
-        <span className="text-xs font-semibold">{message.senderUsername}</span>
-        <span className="text-xs text-zinc-400">
-          {formatTimestamp(message.timestamp)}
-        </span>
-      </div>
-      <span className="w-fit rounded-2xl bg-zinc-200 px-2 py-1 text-sm text-black">
-        {message.textContent}
-      </span>
-    </li>
-  );
-};
+import { ChatMessage } from './components/ChatMessage';
 
 export const ChatView: FC = () => {
   const { chatId } = useLoader({ from: chatRoute.id });
