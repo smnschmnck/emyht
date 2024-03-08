e6e09837ee655450a9701b298176fdf43638a5e9
smnschmnck <simon.schmeinck@gmx.de>
2024-03-08 19:14:19 +0100

    Add file previews

--- frontend/src/components/ui/FilePickerButton.tsx
+++ frontend/src/components/ui/FilePickerButton.tsx
@@ -2,12 +2,29 @@ import { FC } from 'react';
 
 type FilePickerButtonProps = {
   id: string;
+  handleFileChange?: (files: FileList) => void;
 };
 
-export const FilePickerButton: FC<FilePickerButtonProps> = ({ id }) => {
+export const FilePickerButton: FC<FilePickerButtonProps> = ({
+  id,
+  handleFileChange,
+}) => {
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const fileList = event.target.files;
+    if (!fileList) return;
+
+    handleFileChange?.(fileList);
+  };
+
   return (
     <div>
-      <input id={id} className="peer sr-only" type="file" multiple />
+      <input
+        onChange={handleChange}
+        id={id}
+        className="peer sr-only"
+        type="file"
+        multiple
+      />
       <label
         htmlFor={id}
         className="flex h-9 cursor-pointer items-center justify-center rounded-lg bg-blue-600 px-4 text-sm font-medium text-white ring-offset-2 transition hover:bg-blue-500 peer-focus:ring-2 peer-focus:ring-blue-600"
--- frontend/src/pages/index/chat/ChatView.tsx
+++ frontend/src/pages/index/chat/ChatView.tsx
@@ -1,5 +1,6 @@
 import { queryKeys } from '@/configs/queryKeys';
 import { useQuery } from '@tanstack/react-query';
+import prettyBytes from 'pretty-bytes';
 import { useLoader } from '@tanstack/react-router';
 import { FC, useEffect, useRef, useState } from 'react';
 import { ChatHeader } from './components/ChatHeader';
@@ -29,7 +30,40 @@ const MessageList: FC<{ chatId: string }> = ({ chatId }) => {
   );
 };
 
+type FilePreviewProps = {
+  file: File;
+};
+
+const FilePreview: FC<FilePreviewProps> = ({ file }) => {
+  const previewUrl = URL.createObjectURL(file);
+
+  return (
+    <div className="h-48 w-64 overflow-hidden rounded-xl border border-zinc-100 shadow-sm">
+      <img
+        src={previewUrl}
+        alt={file.name}
+        className="h-3/5 w-full bg-gray-300 object-cover"
+      />
+      <div className="flex flex-col gap-1 p-3 text-sm">
+        <div className="flex items-center justify-between font-semibold">
+          <p>{file.name}</p>
+          <p>{prettyBytes(file.size)}</p>
+        </div>
+        <p className="text-zinc-500">{file.type}</p>
+      </div>
+    </div>
+  );
+};
+
 const FilePicker: FC = () => {
+  const [files, setFiles] = useState<File[]>([]);
+
+  const handleFileChange = (fileList: FileList) => {
+    const fileArr = [...files, ...Array.from(fileList)];
+
+    setFiles(fileArr);
+  };
+
   return (
     <div className="flex h-20 w-full grow flex-col p-8">
       <div className="flex w-full justify-between border-b px-2 pb-3">
@@ -38,7 +72,15 @@ const FilePicker: FC = () => {
           <Button variant="text">Deselect X files</Button>
           <Button variant="text">Remove X files</Button>
         </div>
-        <FilePickerButton id="chatFilePicker" />
+        <FilePickerButton
+          id="chatFilePicker"
+          handleFileChange={handleFileChange}
+        />
+      </div>
+      <div className="pt-12">
+        {files.map((file) => (
+          <FilePreview file={file} />
+        ))}
       </div>
     </div>
   );
