0898d9639730b5bacc55312e9b4d2d227dce7151
smnschmnck <simon.schmeinck@gmx.de>
2024-04-03 17:11:52 +0200

    Try fix router icon button

--- frontend/src/components/ui/ButtonLink.tsx
+++ frontend/src/components/ui/ButtonLink.tsx
@@ -1,13 +1,15 @@
 import { Link, MakeLinkOptions } from '@tanstack/react-router';
-import { FC } from 'react';
+import { FC, ReactNode } from 'react';
 import { twMerge } from 'tailwind-merge';
 
-export const ButtonLink: FC<MakeLinkOptions> = (props) => (
+export const ButtonLink: FC<MakeLinkOptions> = ({ children, ...props }) => (
   <Link
     {...props}
     className={twMerge(
       'inline-flex h-9 w-9 items-center justify-center rounded-md bg-white p-2 text-blue-600 transition hover:bg-blue-100',
       props.className
     )}
-  />
+  >
+    <div className="h-full w-full">{children as ReactNode}</div>
+  </Link>
 );
