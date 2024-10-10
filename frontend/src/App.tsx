bd21d21e675a116be93276d8fec1f57a2dbbc8b8
smnschmnck <simon.schmeinck@gmx.de>
2024-10-10 17:07:11 +0200

    Add pusher context

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,8 @@ import { router } from '@/router/config';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider } from '@tanstack/react-router';
 import { Toaster } from 'sonner';
+import Pusher from 'pusher-js';
+import { createContext, useContext } from 'react';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -11,12 +13,26 @@ const queryClient = new QueryClient({
   },
 });
 
+const pusher = new Pusher('fee65361c492db024af1', {
+  cluster: 'us3',
+});
+
+const PusherContext = createContext({
+  pusher,
+});
+
+export const usePusher = () => {
+  return useContext(PusherContext);
+};
+
 const App = () => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Toaster richColors position="top-right" />
-      <RouterProvider router={router} />
-    </QueryClientProvider>
+    <PusherContext.Provider value={{ pusher }}>
+      <QueryClientProvider client={queryClient}>
+        <Toaster richColors position="top-right" />
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </PusherContext.Provider>
   );
 };
 
