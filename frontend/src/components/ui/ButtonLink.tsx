b58526df907e6247890fea218044708d5e5cf59f
smnschmnck <simon.schmeinck@gmx.de>
2024-10-12 15:33:46 +0200

    Fix forwardRef bug

--- frontend/src/components/ui/ButtonLink.tsx
+++ frontend/src/components/ui/ButtonLink.tsx
@@ -1,5 +1,5 @@
 import { createLink } from '@tanstack/react-router';
-import { ReactNode } from 'react';
+import { forwardRef, ReactNode } from 'react';
 import { twMerge } from 'tailwind-merge';
 
 type ExtraProps = {
@@ -8,16 +8,19 @@ type ExtraProps = {
   'aria-label': string;
 };
 
-const ButtonLinkWrapper = ({ children, className, ...props }: ExtraProps) => (
-  <div
-    className={twMerge(
-      'inline-flex h-9 w-9 cursor-pointer items-center justify-center rounded-md bg-white p-2 text-blue-600 transition hover:bg-blue-100',
-      className
-    )}
-    {...props}
-  >
-    <div className="h-full w-full">{children as ReactNode}</div>
-  </div>
+const ButtonLinkWrapper = forwardRef<HTMLDivElement, ExtraProps>(
+  ({ children, className, ...props }, ref) => (
+    <div
+      ref={ref}
+      className={twMerge(
+        'inline-flex h-9 w-9 cursor-pointer items-center justify-center rounded-md bg-white p-2 text-blue-600 transition hover:bg-blue-100',
+        className
+      )}
+      {...props}
+    >
+      <div className="h-full w-full">{children}</div>
+    </div>
+  )
 );
 
 export const ButtonLink = createLink(ButtonLinkWrapper);
