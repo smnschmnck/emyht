bb70f8362ae21fd664a4efe50dfc37224ee3f147
smnschmnck <simon.schmeinck@gmx.de>
2024-11-01 01:55:43 +0100

    Preload fonts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,29 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
+import Unfonts from 'unplugin-fonts/vite';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 //@ts-ignore
 import './src/env';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    Unfonts({
+      fontsource: {
+        families: [
+          'Plus Jakarta Sans Variable',
+          {
+            name: 'Plus Jakarta Sans Variable',
+            weights: [],
+            styles: ['italic', 'normal'],
+            subset: 'latin-ext',
+          },
+        ],
+      },
+    }),
+  ],
   server: {
     port: 3000,
   },
