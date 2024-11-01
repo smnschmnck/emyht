6b471c65a963c9b4d16f4a12e36daaf590089952
smnschmnck <simon.schmeinck@gmx.de>
2024-11-01 16:08:48 +0100

    Prevent balloon flash on mobile nav

--- frontend/src/pages/index/indexPage/IndexPage.tsx
+++ frontend/src/pages/index/indexPage/IndexPage.tsx
@@ -19,7 +19,7 @@ export const IndexPage: FC = () => {
   const hasContactRequests = !!data && data.length > 0;
 
   return (
-    <div className="flex h-full w-full flex-col items-center justify-around px-32">
+    <div className="hidden h-full w-full flex-col items-center justify-around px-32 lg:flex">
       <div className="flex flex-col items-center gap-4">
         <img className="pointer-events-none w-24" src={emyhtLogo} alt="emyht" />
         <div className="text-center">
