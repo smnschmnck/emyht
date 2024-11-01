7bdefe112175b95c5afc91e3e1b83fd65027e231
smnschmnck <simon.schmeinck@gmx.de>
2024-11-01 15:56:05 +0100

    Improve isSidebarHidden hook

--- frontend/src/pages/index/layout/hooks.ts
+++ frontend/src/pages/index/layout/hooks.ts
@@ -1,32 +1,13 @@
-import {
-  useMatchRoute,
-  useRouter,
-  useRouterState,
-} from '@tanstack/react-router';
-
-const CHAT_SUBROUTE = '/chat/';
+import { useMatchRoute } from '@tanstack/react-router';
 
 export const useIsSidebarHidden = () => {
-  const { routesByPath } = useRouter();
-  const { location } = useRouterState();
+  const matchRoute = useMatchRoute();
 
   const hideRoutes = [
-    routesByPath['/initiate'].fullPath,
-    routesByPath['/incoming-requests'].fullPath,
-    CHAT_SUBROUTE,
+    matchRoute({ to: '/chat/$chatId' }),
+    matchRoute({ to: '/initiate' }),
+    matchRoute({ to: '/incoming-requests' }),
   ];
 
-  const idx = hideRoutes.findIndex((r) => location.pathname.startsWith(r));
-
-  return idx !== -1;
-};
-
-export const useChatId = () => {
-  const matchRoute = useMatchRoute();
-  const chatRoute = matchRoute({ to: '/chat/$chatId' });
-  if (!chatRoute) {
-    return;
-  }
-
-  return chatRoute.chatId;
+  return hideRoutes.some((isRouteMatch) => Boolean(isRouteMatch));
 };
