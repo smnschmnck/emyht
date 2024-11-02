94d3319b8ad12980ebc2ab414e011f14d2c59be3
smnschmnck <simon.schmeinck@gmx.de>
2024-11-02 01:20:28 +0100

    Truncate user info

--- frontend/src/pages/index/layout/components/UserInfoSection.tsx
+++ frontend/src/pages/index/layout/components/UserInfoSection.tsx
@@ -9,9 +9,9 @@ export const UserInfoSection: FC = () => {
 
   return (
     <div className="flex h-24 w-full items-center justify-between bg-blue-600 px-6 text-white">
-      <div className="flex flex-col gap-1 text-sm">
-        <p className="font-semibold">{userData?.username ?? ''}</p>
-        <p>{userData?.email ?? ''}</p>
+      <div className="flex w-2/3 flex-col gap-1 text-sm">
+        <p className="truncate font-semibold">{userData?.username ?? ''}</p>
+        <p className="truncate">{userData?.email ?? ''}</p>
       </div>
       <div className="flex gap-1">
         <IconButton ariaLabel="Settings" className="text-white">
