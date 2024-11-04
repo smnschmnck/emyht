8c4ce8cecf5db8d4c0f5256b0e60189813d60684
smnschmnck <simon.schmeinck@gmx.de>
2024-11-04 19:18:16 +0100

    Re-Enable hmr

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import '@fontsource-variable/plus-jakarta-sans';
+import { registerSW } from 'virtual:pwa-register';
 import './index.css';
+
+registerSW({ immediate: true });
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
