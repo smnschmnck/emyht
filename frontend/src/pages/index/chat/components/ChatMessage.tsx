1955145d039a458ba44b3801074f7175b3c2a024
Simon Schmeinck <simon.schmeinck@gmx.de>
2025-02-21 02:22:42 +0100

    display documents in chat

--- frontend/src/pages/index/chat/components/ChatMessage.tsx
+++ frontend/src/pages/index/chat/components/ChatMessage.tsx
@@ -1,14 +1,30 @@
 import { ChatMessage as ChatMessageType } from '@/hooks/api/messages';
 import { useUserData } from '@/hooks/api/user';
 import { formatTimestamp } from '@/utils/dateUtils';
+import { DocumentIcon } from '@heroicons/react/24/outline';
 import { FC } from 'react';
 
 const MessageContent = ({ message }: { message: ChatMessageType }) => (
   <span className="flex w-fit max-w-40 flex-col">
     {message.messageType === 'image' && (
-      <span className="p-1">
-        <img className="overflow-clip rounded-xl" src={message.mediaUrl} />
-      </span>
+      <div className="p-1">
+        <a target="_blank" rel="noopener noreferrer" href={message.mediaUrl}>
+          <img className="overflow-clip rounded-xl" src={message.mediaUrl} />
+        </a>
+      </div>
+    )}
+    {message.messageType === 'data' && (
+      <div className="h-24 w-40 p-1">
+        <div className="flex h-full w-full flex-col items-center justify-center rounded-xl bg-white text-black">
+          <DocumentIcon className="h-10 w-10" />
+          <a
+            className="text-sm text-blue-600 hover:underline"
+            href={message.mediaUrl}
+          >
+            Download File
+          </a>
+        </div>
+      </div>
     )}
     {!!message.textContent && (
       <span className="px-3 py-1.5 text-sm">{message.textContent}</span>
