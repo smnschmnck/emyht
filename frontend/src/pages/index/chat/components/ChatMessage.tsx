20a4c3615a1a2011360cf703aecb020a387d4ea0
Simon Schmeinck <simon.schmeinck@gmx.de>
2025-02-22 01:39:43 +0100

    improve media message design

--- frontend/src/pages/index/chat/components/ChatMessage.tsx
+++ frontend/src/pages/index/chat/components/ChatMessage.tsx
@@ -1,11 +1,11 @@
 import { ChatMessage as ChatMessageType } from '@/hooks/api/messages';
 import { useUserData } from '@/hooks/api/user';
 import { formatTimestamp } from '@/utils/dateUtils';
-import { DocumentIcon } from '@heroicons/react/24/outline';
+import { DocumentArrowDownIcon } from '@heroicons/react/24/outline';
 import { FC } from 'react';
 
-const MessageContent = ({ message }: { message: ChatMessageType }) => (
-  <span className="flex w-fit flex-col">
+const MediaContent = ({ message }: { message: ChatMessageType }) => (
+  <>
     {message.messageType === 'image' && (
       <div className="max-w-48 p-1">
         <a target="_blank" rel="noopener noreferrer" href={message.mediaUrl}>
@@ -14,17 +14,11 @@ const MessageContent = ({ message }: { message: ChatMessageType }) => (
       </div>
     )}
     {message.messageType === 'data' && (
-      <div className="h-24 w-40 p-1">
-        <div className="flex h-full w-full flex-col items-center justify-center rounded-xl bg-white text-black">
-          <DocumentIcon className="h-10 w-10" />
-          <a
-            className="text-sm text-blue-600 hover:underline"
-            href={message.mediaUrl}
-          >
-            Download File
-          </a>
+      <a href={message.mediaUrl} className="group h-28 w-48 p-1">
+        <div className="flex h-full w-full flex-col items-center justify-center rounded-xl bg-zinc-50 text-black transition hover:bg-zinc-100">
+          <DocumentArrowDownIcon className="h-10 w-10" />
         </div>
-      </div>
+      </a>
     )}
     {message.messageType === 'audio' && (
       <audio controls src={message.mediaUrl} />
@@ -32,14 +26,11 @@ const MessageContent = ({ message }: { message: ChatMessageType }) => (
     {message.messageType === 'video' && (
       <div className="max-w-48 p-1">
         <a target="_blank" rel="noopener noreferrer" href={message.mediaUrl}>
-          <video src={message.mediaUrl} />
+          <video className="overflow-clip rounded-xl" src={message.mediaUrl} />
         </a>
       </div>
     )}
-    {!!message.textContent && (
-      <span className="px-3 py-1.5 text-sm">{message.textContent}</span>
-    )}
-  </span>
+  </>
 );
 
 export const ChatMessage: FC<{ message: ChatMessageType }> = ({ message }) => {
@@ -52,9 +43,14 @@ export const ChatMessage: FC<{ message: ChatMessageType }> = ({ message }) => {
   if (data.uuid === message.senderID) {
     return (
       <li className="flex w-full flex-col items-end gap-1">
-        <span className="w-fit rounded-2xl bg-blue-600 text-white">
-          <MessageContent message={message} />
-        </span>
+        {message.messageType !== 'plaintext' && (
+          <MediaContent message={message} />
+        )}
+        {!!message.textContent && (
+          <span className="w-fit rounded-2xl bg-blue-600 px-3 py-1.5 text-sm text-white">
+            {message.textContent}
+          </span>
+        )}
         <span className="text-xs text-zinc-400">
           {formatTimestamp(message.timestamp)}
         </span>
@@ -70,9 +66,14 @@ export const ChatMessage: FC<{ message: ChatMessageType }> = ({ message }) => {
           {formatTimestamp(message.timestamp)}
         </span>
       </div>
-      <span className="w-fit rounded-2xl bg-zinc-100 text-black">
-        <MessageContent message={message} />
-      </span>
+      {message.messageType !== 'plaintext' && (
+        <MediaContent message={message} />
+      )}
+      {!!message.textContent && (
+        <span className="w-fit rounded-2xl bg-zinc-100 px-3 py-1.5 text-sm text-black">
+          {message.textContent}
+        </span>
+      )}
     </li>
   );
 };
